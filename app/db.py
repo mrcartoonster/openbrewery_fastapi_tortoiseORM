898989ac4f27b199295806a7b7e092feb5443093
Evan Baird <mrcartoonster@gmail.com>
2020-10-30 20:49:52 -0600

    📦 NEW: BEER-7 #time 30m #comment beginning query training #in

--- app/db.py
+++ app/db.py
@@ -12,6 +12,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from logger import logger
+from tortoise import Tortoise, run_async
 from tortoise.contrib.fastapi import register_tortoise
 
 env_path = Path("..") / ".env"
@@ -39,4 +40,15 @@ async def generate_schema() -> None:
     Generating schemas for Tortoise model.
     """
     logger.info("Initializing Tortoise!")
-    pass
+
+    await Tortoise.init(
+        db_url=DATABASE_URL,
+        modules={"models": ["models.tortoise"]},
+    )
+    logger.info("Generating database schema")
+    await Tortoise.generate_schemas()
+    await Tortoise.close_connections()
+
+
+if __name__ == "__main__":
+    run_async(generate_schema())
