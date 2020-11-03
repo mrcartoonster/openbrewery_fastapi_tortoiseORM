e7187c5f0d8afe93fb53d9f155f4fa9838606488
Evan Baird <mrcartoonster@gmail.com>
2020-11-03 10:20:47 -0700

    📦 NEW: BEER-4 #time 30m #comment Setup of pytests #in

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,14 @@
 """
 Pytest fixtures and dummy data.
 """
+from typing import Generator
+
+import pytest
+
+
+@pytest.fixture(scope="module")
+def client() -> Generator:
+    """
+    Setup and Teardown of Tortoise.
+    """
+    pass
