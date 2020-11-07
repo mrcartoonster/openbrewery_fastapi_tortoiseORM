2f961665d19ed9dac5d238b8da94fef3c5a56539
Evan Baird <mrcartoonster@gmail.com>
2020-11-07 15:46:45 -0700

    📦 NEW: BEER-4 #time 30m #comment Still building out model Pydantci schema

--- app/routers/schemas.py
+++ app/routers/schemas.py
@@ -2,7 +2,26 @@
 """
 Brewery pydantic schema model.
 """
-from pydantic import BaseModel
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel, HttpUrl
+
+
+class BrewType(str, Enum):
+    """
+    Accepted values in brewery brewery_type field.
+    """
+
+    closed = "closed"
+    regional = "regional"
+    micro = "micro"
+    contract = "contract"
+    brewpub = "brewpub"
+    proprietor = "proprietor"
+    bar = "bar"
+    planning = "planning"
+    large = "large"
 
 
 class BrewerySchema(BaseModel):
@@ -10,4 +29,14 @@ class BrewerySchema(BaseModel):
     Pydantic model for Brewery Response.
     """
 
+    id: str
     name: str
+    brewery_type: BrewType
+    street: Optional[str] = None
+    address_2: Optional[str] = None
+    address_3: Optional[str] = None
+    city: str
+    state: Optional[str] = None
+    country_province: Optional[str] = None
+    postal_code: str
+    website_url: Optional[HttpUrl] = None
