52aacbfb18bf5931b59d5aa841d312c8d64597f9
Evan Baird <mrcartoonster@gmail.com>
2020-11-18 11:19:27 -0700

    📦 NEW: BEER-12 #time 30m #comment removing files for ormar setup #in

--- app/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-initialize for models.
-"""
--- app/routers/crud.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# app/routers/crud.py
-"""
-SQL helper functions for FastAPI endpoint functions.
-"""
