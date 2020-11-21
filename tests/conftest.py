619b923069d69ca122834d4df99772026ea96c97
Evan Baird <mrcartoonster@gmail.com>
2020-11-21 11:59:34 -0700

    📦 NEW: BEER-15 #time 30m #comment Adding tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,3 +3,49 @@
 """
 Pytest fixtures and dummy data.
 """
+import pytest
+from environs import Env
+from fastapi.testclient import TestClient
+from tortoise.contrib.fastapi import register_tortoise
+
+from app.config import Settings, get_settings
+from app.main import create_app
+
+env = Env()
+env.read_env()
+
+
+def get_settings_override():
+    return Settings(testing=1, database_url=env("TEST_DB"))
+
+
+@pytest.fixture(scope="module")
+def test_app():
+    """
+    Setup/Teardown.
+    """
+    app = create_app()
+    app.dependency_overrides[get_settings] = get_settings_override
+
+    with TestClient(app) as test_client:
+
+        yield test_client
+
+
+@pytest.fixture(scope="module")
+def test_app_with_db():
+    """
+    Getting db to test.
+    """
+    app = create_app()
+    app.dependency_overrides[get_settings] = get_settings_override
+    register_tortoise(
+        app,
+        db_url=env("TEST_DB"),
+        modules={"models": ["app.models.tortoise"]},
+        generate_schemas=True,
+        add_exception_handlers=True,
+    )
+    with TestClient(app) as test_client:
+
+        yield test_client
---
+++ tests/test_breweries.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Test location for all brewery endpoints.
+"""
+
+
+def test_get_city(test_app_with_db):
+    """
+    Get 200 response when requesting a city.
+    """
+    pass
