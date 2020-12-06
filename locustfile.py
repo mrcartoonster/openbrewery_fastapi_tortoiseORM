83e113e9a20217c92c2d779c9e445ec9b0e08def
Evan Baird <mrcartoonster@gmail.com>
2020-12-06 12:27:55 -0700

    📦 NEW: BEER-28 #time 30m #comment Working locust file

--- locustfile.py
+++ locustfile.py
@@ -2,3 +2,21 @@
 """
 Stress tests with locusts.
 """
+from locust import HttpUser, between, task
+
+
+class OpenBreweryUser(HttpUser):
+    """
+    This user will query all breweries endpoints.
+    """
+
+    # Having this user query every 5-15 seconds
+    wait_time = between(5, 15)
+
+    @task()
+    def breweries(self):
+        """
+        This query will just be hitting the breweries endpoint with no
+        query parameters.
+        """
+        self.client.get("/breweries")
