a343a0fda5dd441bcb4ce1b632e676278a50a4f5
Evan Baird <mrcartoonster@gmail.com>
2020-12-08 11:01:00 -0700

    📦 NEW: BEER-24 #time 30m #comment Gunicorn and Uvicorn work locally with the gunicorn.config.py file

--- gunicorn.config.py
+++ gunicorn.config.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
+# gunicorn.config.py
 """
 Gunicorn Uvicorn config to lauch in Digital Ocean's App Platform.
 
 Using their Flask template: https://github.com/digitalocean/sample-flask
 
 """
-import uvicorn
 
 bind = "0.0.0.0:8080"
 workers = 2
-worker_class = uvicorn.workers.UvicornWorker
+# Using Uvicorn's Gunicorn worker class
+worker_class = "uvicorn.workers.UvicornWorker"
